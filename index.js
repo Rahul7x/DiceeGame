419516717e534987e65ad70041332375d9920508
Rahul Sharma <150765155+Rahul7x@users.noreply.github.com>
2025-04-24 01:47:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ document.querySelector(".img1").setAttribute("src", player1);
 document.querySelector(".img2").setAttribute("src", player2);
 
 if (player1>player2) {
-    document.querySelector("h1").textContent = "Player 1 Wins!" ;
+    document.querySelector("h1").textContent = " 🏁 Player 1 Wins!" ;
 } else if (player2>player1) {
-    document.querySelector("h1").textContent = "Player 2 Wins!" ;
+    document.querySelector("h1").textContent = "Player 2 Wins! 🏁 " ;
 } else {
-    document.querySelector("h1").textContent = "It's a Draw, Refresh me Again for a Winner" ;
+    document.querySelector("h1").textContent = "It's a Draw, Refresh Again for a Winner" ;
 }
