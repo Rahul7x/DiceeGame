2ba595ddf9bde6b42a59e7e438791b4035d7a901
Rahul Sharma <150765155+Rahul7x@users.noreply.github.com>
2025-04-24 02:11:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var player1 = "./images/dice" + ((Math.floor(Math.random()*6))+1) + ".png";
-var player2 = "./images/dice" + ((Math.floor(Math.random()*6))+1) + ".png";
+var randomNum1 = Math.floor (Math.random() * 6) + 1;
+var randomNum2 = Math.floor (Math.random() * 6) + 1;
 
-document.querySelector(".img1").setAttribute("src", player1);
-document.querySelector(".img2").setAttribute("src", player2);
+var image1 = "./images/dice" + randomNum1 + ".png"
+var image2 = "./images/dice" + randomNum2 + ".png"
 
-if (player1>player2) {
+document.querySelector(".img1").setAttribute("src", image1);
+document.querySelector(".img2").setAttribute("src", image2);
+
+if (randomNum1 > randomNum2) {
     document.querySelector("h1").textContent = " 🏁 Player 1 Wins!" ;
-} else if (player2>player1) {
+} 
+else if (randomNum1 < randomNum2) {
     document.querySelector("h1").textContent = "Player 2 Wins! 🏁 " ;
-} else {
+} 
+else {
     document.querySelector("h1").textContent = "It's a Draw, Refresh Again for a Winner" ;
 }
